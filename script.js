993f372684e05607cf879685f861e6a35c8a9444
mohiitsingh <ms6065097@gmail.com>
2024-05-01 13:49:08 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const searchInput = document.querySelector('.search-item');
+    const searchIcon = document.querySelector('.icon');
+
+    // Add click event listener to search input
+    searchInput.addEventListener('click', function() {
+        // Hide search icon
+        searchIcon.style.display = 'none';
+    });
+});
